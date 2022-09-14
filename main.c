7f11eaa8f1483e33612d5d4a70524007b25f5b4e
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-09-14 19:08:01 +0800

    ray weekend no.4

--- main.c
+++ main.c
@@ -6,12 +6,14 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:33:51 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/09/13 20:48:43 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/09/14 16:44:13 by leng-chu         ###   ########.fr       */
 /*   Updated: 2021/12/07 11:48:40 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+#include <unistd.h>
+#include <stdio.h>
 
 typedef struct s_data
 {
@@ -30,6 +32,20 @@ void	ft_pixel(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	deal_key(int key, void *param)
+{
+	(void)key;
+	int *test = param;
+	ft_putchar('X');
+	printf("param: %d\n", *test);
+	return (0);
+}
+
 int	main(void)
 {
 	void	*mlx;
@@ -44,6 +60,9 @@ int	main(void)
 		for (int j = 0; j < 300; j++)
 			ft_pixel(&img, i, j, 0x00FF0000); // pixel positon is on image map
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 100, 0); // image positon is on window map
+
+	int lol = 42;
+	mlx_key_hook(mlx_win, deal_key, &lol);
 	mlx_loop(mlx);
 	(void)mlx_win;
 	return (0);
